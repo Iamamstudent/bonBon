c6990e74f4f64c5737929ff0cca785e849f7ee0c
Yewon <17chlorinegoat@gmail.com>
2021-12-22 21:47:50 +0900

    some pages deleted

--- questions_html/nextPage.js
+++ questions_html/nextPage.js
@@ -1,4 +1,4 @@
-const linkList = ["studentId.html", "phoneNum.html", "track.html", "sex.html", "sleepComment.html", "sleepTime.html", "sleepHabit.html", "sleepSense.html", "cleanComment.html", "trash.html", "clean.html", "othersComment.html", "studyPlace.html", "noise.html", "bathTime.html", "insectHunt.html", "finalComment.html", "mbti.html", "endingComment.html"]
+const linkList = ["studentId.html", "phoneNum.html", "track.html", "sex.html", "sleepComment.html", "sleepTime.html", "sleepHabit.html", "cleanComment.html", "trash.html", "othersComment.html", "studyPlace.html", "noise.html", "bathTime.html", "finalComment.html", "insectHunt.html", "endingComment.html"]
 function nextPage(sth) {
     if (window.event.keyCode == 13) {
         location.href=linkList[sth];
