9dd930a915736284827a6503ebfcc5a9b5538207
Yewon <17chlorinegoat@gmail.com>
2021-12-27 22:41:05 +0900

    html connections are moved to nextPage.js

--- questions/nextPage.js
+++ questions/nextPage.js
@@ -1,6 +1,22 @@
+const app = require("../app");
+
 const linkList = ["studentId.html", "phoneNum.html", "track.html", "sex.html", "sleepComment.html", "sleepTime.html", "sleepHabit.html", "cleanComment.html", "trash.html", "othersComment.html", "studyPlace.html", "noise.html", "bathTime.html", "finalComment.html", "insectHunt.html", "endingComment.html"]
+const listName = ["index", "studentId", "phoneNum", "track", "sex", "sleepComment", "sleepTime", "sleepHabit", "cleanComment", "trash", "othersComment", "studyPlace", "noise", "bathTime", "finalComment", "insectHunt", "endingComment"]
+function buttonNext(sth, res) {location.href=linkList[sth]};
+
 function nextPage(sth) {
-    if (window.event.keyCode == 13) {
+    var x = document.forms["inputTextKeyup"][listName[sth]].value;
+    if (x == "") {
+    alert("Below must be filled out");
+    return false;
+    } else if (listName[sth-1] == "SleepHabit") {
+        var x1 = document.forms["inputTextKeyup"]["SleepTalk"];
+        var x2 = document.forms["inputTextKeyup"]["SleepSnore"];
+        var x3 = document.forms["inputTextKeyup"]["SleepTeeth"];
+        if (x1 == "" || x2 == "" || x3 == "") {
+            alert("Below must be filled out")
+        }
+    } else if (window.event.keyCode == 13) {
         location.href=linkList[sth];
     }
 }
